32bbbb1f8de260a9629348eb07a8cbf5c8eac433
ezanyanwu <ezeanyinabia.anyanwu@gmail.com>
2020-09-30 18:39:36 -0400

    Support multiple hosts

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ import { Socket } from 'dgram';
 const log = console.log;
 const STATUS_DOWN = "DOWN";
 const STATUS_UP = "UP";
-const statusTemplate = {};
+const statuses = [];
 
 if (process.argv.length < 4) {
-    log("usage: node ./index.js <host> <port> [...<additional_ports>]");
+    log("usage: node ./index.js <host>|<host>[|<additional_hosts>] <port>|<port>[|<additional_ports>]");
     process.exit(1);
 }
 
-const host = process.argv[2];
-const portStrings = process.argv.slice(3);
+const hosts = process.argv[2].split('|');
+const portStrings = process.argv[3].split('|');
 // Validate ports
 portStrings.forEach(s => {
     const p = Number.parseInt(s);
@@ -23,91 +23,123 @@ portStrings.forEach(s => {
         process.exit(1);
     }
 });
-
 const ports = portStrings.map(x => Number.parseInt(x));
 
-statusTemplate['host'] = host;
+const dnsLookups = [];
 
-// DNS lookup
+for (const host of hosts) {
+    let lookup = {};
+    dnsLookups.push(lookup);
+    // DNS lookup
 
-let startDns4, startDns6;
-let endDns4, endDns6;
-let records4, records6;
+    let startDns4, startDns6;
+    let endDns4, endDns6;
+    let records4, records6;
 
-try {
-    startDns6 = performance.now();
-    records6 = await dns.resolve6(host);
-    endDns6 = performance.now();
-} catch (e) {
-    // no ipv6 records
+    try {
+        startDns6 = performance.now();
+        records6 = await dns.resolve6(host);
+        endDns6 = performance.now();
+    } catch (e) {
+        // no ipv6 records
+    }
+
+    try {
+        startDns4 = performance.now();
+        records4 = await dns.resolve4(host);
+        endDns4 = performance.now();
+    } catch (e) {
+        // No ipv4 records    
+    }
+
+    if (!records4 && !records6) {
+        lookup['status'] = STATUS_DOWN;
+        lookup['reason'] = `DNS lookup error. No records for ${host}`;
+        continue;
+    }
+
+    let address;
+    let dnsDelay;
+
+    if (records4) {
+        address = records4[0];
+        dnsDelay = endDns4 - startDns4;
+    } else {
+        address = records6[0];
+        dnsDelay = endDns6 - startDns6;
+    }
+
+    lookup['dns-delay-ms'] = dnsDelay;
+    lookup['host'] = host;
+    lookup['address'] = address;
+
+
+    for (const port of ports) {
+        statuses.push({
+            port: port,
+            ...lookup
+        });
+    }
 }
 
-try {
-    startDns4 = performance.now();
-    records4 = await dns.resolve4(host);
-    endDns4 = performance.now();
-} catch (e) {
-    // No ipv4 records    
-}
+let sockets = [];
 
-if (!records4 && !records6) {
-    statusTemplate['status'] = STATUS_DOWN;
-    statusTemplate['reason'] = `DNS lookup error. No records for ${host}`;
-    log(status);
-    process.exit(1);
-}
+for (let status of statuses) {
 
-let address;
-let dnsDelay;
+    if (status.status === STATUS_DOWN) {
+        continue;
+    }
 
-if (records4) {
-    address = records4[0];
-    dnsDelay = endDns4 - startDns4;
-} else {
-    address = records6[0];
-    dnsDelay = endDns6 - startDns6;
-}
-
-statusTemplate['dns-delay-ms'] = dnsDelay;
-statusTemplate['address'] = address;
-
-
-for (let port of ports) {
-    let status = Object.assign({}, statusTemplate);
-    status['port'] = port;
-    
     // TCP Connection
     const connectionOptions = {
-        'port': port,
-        'host': address,
-        'family': 0,
+        'port': status.port,
+        'host': status.address,
     };
     
-    let connectStart = performance.now();
+    const connectStart = performance.now();
     
     let s = new net.Socket()
                     .setTimeout(5 * 1000)
                     .connect(connectionOptions);
     
     s.on('connect', () => {
-        let connectEnd = performance.now();
+        const connectEnd = performance.now();
         status['connection-delay-ms'] = connectEnd - connectStart;
         status["status"] = STATUS_UP;
-        log(status);
         s.end();
     });
     
     s.on('timeout', () => {
-        status['status'] = STATUS_DOWN;
-        status['reason'] = 'Timeout';
-        log(status);
+        status.status = STATUS_DOWN;
+        status.reason = 'Timeout';
+        // THere is nothing at the other end, so destroy immediately
         s.destroy();
     });
     
     s.on('error', (e) => {
-        status['status'] = STATUS_DOWN;
-        status['reason'] = e,
-        log(status);
-        // the socket code calls close() after an 'error' event.
+        status.status = STATUS_DOWN;
+        status.reason = e;
+        // `close` event will be sent right after this;
     });
+
+    s.on('close', () => {
+        s.destroy();
+    });
+
+    s.on('end', () => {
+        s.destroy();
+    });
+
+    sockets.push(s);
 }
+
+
+const fun = () => {
+    if (sockets.every(x => x.destroyed)) {
+        log(statuses);
+    } else {
+        setTimeout(fun, 500);
+    }
+};
+
+setTimeout(fun, 500);
