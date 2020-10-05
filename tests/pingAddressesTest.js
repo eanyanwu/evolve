270aa7d5574c1de7f2f11a20615c9f8c41819a57
ezanyanwu <ezeanyinabia.anyanwu@gmail.com>
2020-10-05 00:24:47 -0400

    Update pingAddresses timeout test

--- tests/pingAddressesTest.js
+++ tests/pingAddressesTest.js
@@ -30,5 +30,18 @@ describe('pingAddresses', () => {
   });
 
   it('Records a timeout error for connections to invalid targets', async () => {
+    const hostname = 'example.org';
+    const port = 1;
+    const address = (await resolveHostnames([hostname]))[0].address;
+    const connectionOption = {
+      hostname,
+      address,
+      port,
+    };
+
+    const result = await pingAddresses([connectionOption], 1_000);
+
+    expect(result.length).to.equal(1);
+    expect(result[0].connectionError).to.equal('Timeout');
   });
 });
