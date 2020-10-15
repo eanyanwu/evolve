20ce0ef82e828c22e1dc4246b5d23bbf859f53ea
ezanyanwu <ezeanyinabia.anyanwu@gmail.com>
2020-10-14 21:42:20 -0400

    Simpler way to make requests

---
+++ simpleRequest.js
@@ -0,0 +1,39 @@
+import http from 'http';
+
+export default function simpleRequest(requestArgs, cb) {
+  const req = http.request(requestArgs);
+
+  req.on('timeout', () => {
+    const err = new Error('Socket timed out');
+    err.code = 'ETIMEDOUT';
+    req.destroy(err);
+  });
+
+  req.on('error', (err) => {
+    cb(err);
+  });
+
+  req.on('response', (res) => {
+    res.on('error', (err) => {
+      cb(err);
+    });
+
+    let data = [];
+    res.on('data', (chunk) => {
+      data.push(chunk);
+    });
+
+    res.on('end', () => {
+      data = Buffer.concat(data);
+      const { statusCode, statusMessage, headers } = res;
+      cb(null, {
+        statusCode,
+        statusMessage,
+        headers,
+        data,
+      });
+    });
+  });
+
+  return req.end();
+};
---
+++ tests/simpleRequestTests.js
@@ -0,0 +1,35 @@
+import util from 'util';
+import { strict as assert } from 'assert';
+import request from '../simpleRequest.js';
+import SimpleHTTPServer from '../simpleHTTPServer.js';
+
+const requestAsync = util.promisify(request);
+
+describe.only('simpleRequest', () => {
+  it('can get an html page', async () => {
+    const result = await requestAsync({
+      host: 'example.org',
+      port: 80,
+      path: '/',
+    });
+
+    const { statusCode, data } = result;
+    assert.equal(statusCode, 200);
+
+    const html = data.toString();
+    assert.equal(html.startsWith('<!doctype html>'), true);
+  });
+
+  it('handles timeouts', (done) => {
+    const req = request({
+      host: 'example.org',
+      port: 79,
+      path: '/',
+      timeout: 1,
+    }, (err, res) => {
+      assert.notEqual(err, null);
+      assert.equal(err.code, 'ETIMEDOUT');
+      done();
+    });
+  });
+});
